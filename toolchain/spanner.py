4bc4c8a4ed7af1a847d849f2509714a16578b96e
KLA <smtwkla@gmail.com>
2024-10-16 08:09:44 +0530

    feat: spanner now supports build and push, uses secrets.py

--- toolchain/spanner.py
+++ toolchain/spanner.py
@@ -1,53 +1,79 @@
 import click
 import os
 import subprocess
+import secrets
 
 
 @click.group
 def cli():
-    pass
+	pass
 
 @click.command()
-def build():
-    click.echo("build")
-    os.system("docker build -t vel_geo_master_dev .")
-    os.system("docker image ls")
+@click.argument('target')
+def build(target):
+	if target == "dev":
+		click.echo("build bench container image for dev")
+		os.system("docker build -t vel_geo_master_dev .")
+		os.system("docker image ls")
+	elif target == "base":
+		click.echo("build frappe_base container image for prod")
+		os.system("docker build -t frappe_base:latest --file=build/Dockerfile build")
+	elif target == "app":
+		click.echo("build vel_geo_master container image for prod")
+		click.echo(secrets.REPO_URI)
+		os.system(f"docker build -t {secrets.APP_IMAGE_NAME}:{secrets.APP_VERSION} --file=build/Dockerfile-app "
+		          f'--build-arg REPO_URI="{secrets.REPO_URI}" '
+		          "build")
+
+@click.command()
+@click.argument('target')
+def push(target):
+	os.system(f"docker login -u {secrets.REPO_USER} -p {secrets.REPO_TOKEN} {secrets.CONTAINER_REPO_SERVER}")
+	if target == "base":
+		click.echo("Not implemented.")
+	elif target == "app":
+		cmd = f"docker image tag {secrets.APP_IMAGE_NAME}:{secrets.APP_VERSION} {secrets.APP_IMAGE_URI}"
+		click.echo(cmd)
+		os.system(cmd)
+		cmd = f"docker image push {secrets.APP_IMAGE_URI}"
+		click.echo(cmd)
+		os.system(cmd)
 
 @click.command()
 @click.option('-f', is_flag=True, help="Force recreation.")
 def configurator(f):
-    click.echo("configurator")
-    os.system("docker compose --profile=init_bench up db redis-cache redis-queue backend configurator"
-              f'{" --force-recreate" if f else ""}'
-              )
+	click.echo("run configurator on dev compose stack")
+	os.system("docker compose --profile=init_bench up db redis-cache redis-queue backend configurator"
+			  f'{" --force-recreate" if f else ""}'
+			  )
 
 @click.command()
 @click.option('-f', is_flag=True, help="Force recreation.")
 def create_site(f):
-    click.echo("create_site")
-    os.system("docker compose --profile=init_bench up db redis-cache redis-queue backend create-site"
-              f'{" --force-recreate" if f else ""}'
-              )
+	click.echo("run create_site on dev compose stack")
+	os.system("docker compose --profile=init_bench up db redis-cache redis-queue backend create-site"
+			  f'{" --force-recreate" if f else ""}'
+			  )
 
 @click.command()
 @click.option('-d', is_flag=True, help="Run in daemon mode.")
 @click.option('-f', is_flag=True, help="Force recreation.")
 def run(d,f):
-    click.echo("run")
-    os.system(f"docker compose up {'-d' if d else ''}"
-              f'{" --force-recreate" if f else ""}'
-              )
+	click.echo("run dev compose stack")
+	os.system(f"docker compose up {'-d' if d else ''}"
+			  f'{" --force-recreate" if f else ""}'
+			  )
 
 @click.command()
 @click.option('-v', is_flag=True, help="Remove container volumes.")
 def stop(v):
-    click.echo("stop")
-    os.system(f"docker compose down {'-v' if v else ''}")
+	click.echo("stop dev compose stack")
+	os.system(f"docker compose down {'-v' if v else ''}")
 
 @click.command()
 def bash():
-    click.echo("bash")
-    os.system("docker compose exec bench bash")
+	click.echo("bash into bench container")
+	os.system("docker compose exec bench bash")
 
 #REM docker run --rm -it --name customer_erp_dev -p 8000:8000 customer_erp_dev
 
@@ -57,5 +83,6 @@ cli.add_command(create_site)
 cli.add_command(run)
 cli.add_command(stop)
 cli.add_command(bash)
+cli.add_command(push)
 if __name__ == '__main__':
-    cli()
+	cli()
