dd8e283ef8c713ad6f628461e657e36efee7151f
KLA <smtwkla@gmail.com>
2024-10-21 23:16:39 +0530

    feat: Power of Attorney option added to document type - can be used for general and special PoA.; feat- dev_install.sh script to build dev container from scratch (test)

--- toolchain/dev_install.sh
+++ toolchain/dev_install.sh
@@ -7,3 +7,12 @@ bench set-config -g redis_cache redis://redis-cache:6379
 bench set-config -g redis_queue redis://redis-queue:6379
 bench set-config -g redis_socketio redis://redis-queue:6379
 git config --global --add safe.directory '*'
+bench get-app vel_geo_master /workspace/
+bench config set-common-config -c root_password '"admin"'
+bench new-site --no-mariadb-socket vel_geo_master_dev
+bench --site vel_geo_master_dev set-config developer_mode 1
+bench --site vel_geo_master_dev clear-cache
+bench --site vel_geo_master_dev install-app vel_geo_master
+bench use vel_geo_master_dev
+rm -Rvf apps/vel_geo_master
+ln -s /workspace apps/vel_geo_master
